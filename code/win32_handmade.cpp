36461d9a155bf1c1cb73a41b44a2df7a1ab41b6e
NagyGeorge <george.nagy0969@outlook.com>
2021-05-27 16:26:30 -0500

    initialization

---
+++ code/win32_handmade.cpp
@@ -0,0 +1,18 @@
+  /*=======================================
+  $FILE: $
+  $DATE: $
+  $REVISION: $
+  $CREATOR: George Nagy $
+  =========================================*/
+
+#include <windows.h>
+
+int CALLBACK
+WinMain(HINSTANCE hInstance,
+        HINSTANCE hPrevInstance,
+        LPSTR     lpCmdLine,
+        int       nShowCmd
+)
+{
+  return(0);
+}
